62a425f673cf95058524350cff94749dd2ff8100
Paul Everitt <pauleveritt@me.com>
2018-08-01 20:00:56 +0200

    Clean up tests.

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,8 +1,16 @@
 from dataclasses import dataclass
+from typing import Dict
 
 import pytest
 from bs4 import BeautifulSoup
 from jinja2 import TemplateSyntaxError
+
+
+def get_soup(ts: str, context: Dict):
+    template = root_environment.from_string(ts)
+    r = template.render(context)
+    soup = BeautifulSoup(r, 'html5lib')
+    return soup
 
 
 @dataclass
@@ -39,15 +47,20 @@ def test_Root_multiple(root_environment):
     # Now assert
     expected = 'Root World children: x=99'
     assert expected == result
-    # result_two = template.render(dict(x=88))
-    # expected = 'Root-()-\n<div>x=88</div>'
-    # assert expected == str.strip(result_two)
+
+    # Second
+    r = template.render(dict(x=89))
+    soup = BeautifulSoup(r, 'html5lib')
+    result = soup.find(id='root').string
+    expected = 'Root World children: x=89'
+    assert expected == result
 
 
 @pytest.mark.parametrize(
     'template_string, expected',
     [
         ('{% Root "one %}{% endRoot %}', TemplateSyntaxError),
+        ('{% Root "one %}', TemplateSyntaxError),
     ]
 )
 def test_Root_fail(root_environment, template_string, expected):
